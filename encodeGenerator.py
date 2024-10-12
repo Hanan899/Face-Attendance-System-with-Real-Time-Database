fd7029e4ce5aa52fcc52e770a114103cb62dfd0a
Abdul Hanan <rao96861@gmail.com>
2024-10-13 04:48:45 +0500

    Add files via upload

---
+++ encodeGenerator.py
@@ -0,0 +1,37 @@
+import cv2 as cv
+import face_recognition
+import pickle
+import os
+
+
+# student face images
+studentFacesPaths = os.listdir('Images')
+
+studentFacesList = []
+studentIDs =[]
+
+for path in studentFacesPaths:
+    studentFacesList.append(cv.imread(os.path.join('Images', path)))
+    studentIDs.append(os.path.splitext(path)[0])
+
+
+def doEncoding(images):
+    encodeList = []
+    for img in images:
+        img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    
+    return encodeList
+
+
+
+print("Encoding ... please wait ... ")
+encodedImages = doEncoding(studentFacesList)
+encodedImagesWithIDs = [encodedImages, studentIDs]
+print("Encoding complete")
+
+file = open('EncodedImages.p', 'wb')
+pickle.dump(encodedImagesWithIDs, file)
+file.close()
+print("File saved")
